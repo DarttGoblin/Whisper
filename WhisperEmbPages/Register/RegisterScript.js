2e13d8053c75e8dd976767c306ed9f993626a806
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2024-08-06 17:48:24 +0100

    Update RegisterScript.js

--- WhisperEmbPages/Register/RegisterScript.js
+++ WhisperEmbPages/Register/RegisterScript.js
@@ -2,6 +2,7 @@ const inputs = document.getElementsByClassName("inputs");
 const select = document.getElementsByTagName("select")[0];
 const register = document.getElementById("register");
 const haveacc = document.getElementById("haveacc");
+const accessDenial = document.getElementById('accessDenial');
 
 for (let i = 0; i < inputs.length; i++) {inputs[i].oninput = function() {inputs[i].value = inputs[i].value.charAt(0).toLowerCase() + inputs[i].value.slice(1);}}
 
@@ -29,7 +30,7 @@ register.onclick = function() {
     .then(data => {
         if (data.success) {
             alert('Registration has been successful!');
-            window.location.href = 'https://darttgoblin.github.io/Whisper/Login.html';
+            window.location.href = '../../Login.html';
             register.innerHTML = 'Register';
         }
         else {
@@ -45,11 +46,24 @@ register.onclick = function() {
 }
 
 function DisplayError(error) {
-    if (error == 'name') {alert('Name is required');}
-    else if (error == 'email') {alert('Email is required');}
-    else if (error == 'username') {alert('username is required');}
-    else if (error == 'password') {alert('password is required!');}
-    else if (error == 'cpassword') {alert('password confirmation is required!');}
-    else if (error == 'gender') {alert('Gender is required!');}
-    else if (error == 'notmatched') {alert('Passwords are not matched!');}
+    if (error == 'name') {AccessDenial('Name is required');}
+    else if (error == 'email') {AccessDenial('Email is required');}
+    else if (error == 'username') {AccessDenial('username is required');}
+    else if (error == 'password') {AccessDenial('password is required!');}
+    else if (error == 'cpassword') {AccessDenial('password confirmation is required!');}
+    else if (error == 'gender') {AccessDenial('Gender is required!');}
+    else if (error == 'notmatched') {AccessDenial('Passwords are not matched!');}
+}
+function AccessDenial(message) {
+    accessDenial.innerHTML = message;
+    register.style.visibility = "hidden";
+    accessDenial.style.visibility = "visible";
+    accessDenial.style.opacity = 1;
+    accessDenial.style.top = "-40px";
+    setTimeout(() => {
+        submit.style.visibility = "visible";
+        accessDenial.style.visibility = "hidden";
+        accessDenial.style.opacity = 0;
+        accessDenial.style.top = "0px";
+    }, 3000);
 }
