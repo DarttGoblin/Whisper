48045e253dabf22c905296fd284648f969f17a6e
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2024-08-08 19:35:37 +0100

    Update GroupStyleScript.js

--- WhisperEmbPages/Group/GroupStyleScript.js
+++ WhisperEmbPages/Group/GroupStyleScript.js
@@ -1,4 +1,3 @@
-
 const loadingCont = document.createElement('div');
 const loadingIconCont = document.createElement('div');
 const loadingIcon = document.createElement('i');
@@ -120,17 +119,6 @@ function GetCurrentTimestamp() {
     const hours = String(currentDate.getHours()).padStart(2, '0');
     const minutes = String(currentDate.getMinutes()).padStart(2, '0');
     
-    return `${year}-${month}-${day} ${hours}:${minutes}`;
-}
-function ConvertDateFormat(dateString) {
-    const date = new Date(Date.parse(dateString));
-
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-
     return `${year}-${month}-${day} ${hours}:${minutes}`;
 }
 function ChangeBackground(gender) {
