ee4996ea6bd02616e324ee7683fb0a64748014da
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2024-08-08 20:01:59 +0100

    Update GroupStyleScript.js

--- WhisperEmbPages/Group/GroupStyleScript.js
+++ WhisperEmbPages/Group/GroupStyleScript.js
@@ -1,3 +1,4 @@
+
 const loadingCont = document.createElement('div');
 const loadingIconCont = document.createElement('div');
 const loadingIcon = document.createElement('i');
@@ -109,17 +110,6 @@ function SaveJoinMessage(username, state) {
         else {console.log(state + 'message not saved!');}
     })
     .catch(error => {console.log(error);});
-}
-function GetCurrentTimestamp() {
-    const currentDate = new Date();
-    
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
-    const hours = String(currentDate.getHours()).padStart(2, '0');
-    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-    
-    return `${year}-${month}-${day} ${hours}:${minutes}`;
 }
 function ChangeBackground(gender) {
     if (gender == 'male') {document.body.style.backgroundImage = maleBackgrounds[Math.floor(Math.random() * maleBackgrounds.length)];}
